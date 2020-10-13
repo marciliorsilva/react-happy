745a4aeefe9d9ef2f24d960eae525ce38a612bea
Marcilio <marciliorsilva93@gmail.com>
2020-10-13 16:03:40 -0300

    criação da pagina inicial e a estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import {FiArrowRight} from 'react-icons/fi'
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt=""/>
+
+        <main>
+          <h1>Leve felecidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+        
+        <div className="location">
+          <strong>São Lourenço da Mata</strong>
+          <span>Pernambuco</span>
+        </div>
+
+        <a href="" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+          
+      </div>
     </div>
   );
 }
